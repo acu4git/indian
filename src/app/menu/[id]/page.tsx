1dbe25e8239f7f050c94652593aa0ea29d055671
Eztas <nietzsche2669sensei@gmail.com>
2025-09-17 23:10:59 +0900

    update: 非同期処理はPromiseにする

--- src/app/menu/[id]/page.tsx
+++ src/app/menu/[id]/page.tsx
@@ -1,14 +1,25 @@
-export default function MenuDetailPage({ params, searchParams }: { params: { id: string }, searchParams: { name?: string, description?: string } }) {
-    if (!params.id) {
+export default async function MenuDetailPage({ 
+    params, 
+    searchParams 
+}: { 
+    // 非同期処理はNext.js15ではPromiseで順番に受け取る
+    params: Promise<{ id: string }>, 
+    searchParams: Promise<{ name?: string, description?: string }> 
+}) {
+    // paramsとsearchParamsを非同期で取得
+    const resolvedParams = await params;
+    const resolvedSearchParams = await searchParams;
+    
+    if (!resolvedParams.id) {
         return <div className="text-white">カレー味の注文は店にお尋ねください。</div>;
     }
 
     return (
         <div className="min-h-screen bg-gray-800 text-white flex flex-col items-center justify-center p-4">
             <div className="border-2 border-cyan-400 p-8 rounded-lg shadow-lg shadow-cyan-400/20">
-                <p className="text-lg text-gray-400 mb-2">{params.id}</p>
-                <h1 className="text-4xl font-bold mb-4">{searchParams.name}</h1>
-                <p className="text-xl">{searchParams.description}</p>
+                <p className="text-lg text-gray-400 mb-2">{resolvedParams.id}</p>
+                <h1 className="text-4xl font-bold mb-4">{resolvedSearchParams.name}</h1>
+                <p className="text-xl">{resolvedSearchParams.description}</p>
             </div>
         </div>
     );
