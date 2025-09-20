4842a116376ebeb13fdff832eb4926af250cf648
Heita Kato <129523656+heita-kato@users.noreply.github.com>
2025-09-20 18:20:43 +0900

    add: 注文時警告文の追加 (#4)

---
+++ src/app/Warning/components/IgnoreButton.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function IgnoreButton() {
+  return (
+    <button
+      className="rounded-lg bg-white border border-red-600 px-4 py-2 text-red-600 font-semibold opacity-80 cursor-default ml-3"
+    >
+      無視する
+    </button>
+  );
+}
---
+++ src/app/Warning/types/index.ts
@@ -0,0 +1,1 @@
+export type WarningMessageType = string;
