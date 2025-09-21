9749b4b8da9963f1b00db544ca00f0bf25af04a1
nrysk <88011751+nrysk@users.noreply.github.com>
2025-09-21 11:01:28 +0900

    feat: カス利用規約のレベルアップ

---
+++ src/app/quiz/page.tsx
@@ -0,0 +1,11 @@
+export default function QuizPage() {
+  return (
+    <div className="flex flex-col w-dvw">
+      <h1 className="text-3xl font-bold mb-6 mx-auto fixed">クイズ</h1>
+      <div className="mt-20 mx-auto text-center">
+        <p className="text-lg">ここにクイズの内容が入ります。</p>
+        {/* クイズの選択肢や結果表示などのコンポーネントを追加 */}
+      </div>
+    </div>
+  );
+}
