2e8b3dfdfcde10fdaa5409417f582c505b4ff383
Eztas <nietzsche2669sensei@gmail.com>
2025-09-21 10:09:19 +0900

    update: 右下部分をカード化&V字矢印部分の追加

--- src/app/Order/components/rightArea.tsx
+++ src/app/Order/components/rightArea.tsx
@@ -27,18 +27,37 @@ export const RightArea = ({ waitingNumbers, myTicketNumber }: RightAreaProps) =>
 
             {/* 右下: 待機中 */}
             <div className="mt-6">
-            <h3 className="font-bold text-gray-700">呼び出し中のお客様番号</h3>
-            <div className="flex flex-wrap gap-3 mt-2 p-3 bg-gray-100 rounded-md border">
-                {/* myTicketNumberを含むdisplayNumbersを使用 */}
-                {displayNumbers.map((num) => (
-                <span 
-                    key={num} 
-                    className={`px-3 py-1 text-lg font-bold rounded-full transition-all duration-300 'bg-gray-300 text-gray-600`}
-                >
-                    {num}
-                </span>
-                ))}
-            </div>
+                <h3 className="font-bold text-gray-700">呼び出し中のお客様番号</h3>
+                <div className="flex items-center mt-2"> {/* V字矢印とカード群を横並びにするためのflexコンテナ */}
+                    {/* 左向きV字矢印を2つ (横に並べてサイズを調整) */}
+                    <div className="flex items-center mr-4"> {/* V字矢印全体を横に並べるためのコンテナ */}
+                        {/* 1つ目の左向きV字矢印 */}
+                        <div className="relative w-8 h-8 mr-[-8px]"> {/* 矢印全体のコンテナサイズ調整、重なり調整 */}
+                            <div className="absolute top-1/2 left-0 w-full h-1 bg-gray-600 transform -translate-y-1/2 -rotate-45 origin-bottom-left"></div> {/* 上側の斜め線 */}
+                            <div className="absolute top-1/2 left-0 w-full h-1 bg-gray-600 transform -translate-y-1/2 rotate-45 origin-top-left"></div> {/* 下側の斜め線 */}
+                        </div>
+                        {/* 2つ目の左向きV字矢印 */}
+                        <div className="relative w-8 h-8"> {/* 矢印全体のコンテナサイズ調整 */}
+                            <div className="absolute top-1/2 left-0 w-full h-1 bg-gray-600 transform -translate-y-1/2 -rotate-45 origin-bottom-left"></div> {/* 上側の斜め線 */}
+                            <div className="absolute top-1/2 left-0 w-full h-1 bg-gray-600 transform -translate-y-1/2 rotate-45 origin-top-left"></div> {/* 下側の斜め線 */}
+                        </div>
+                    </div>
+                    
+                    <div className="flex items-center p-3 bg-gray-100 rounded-md border overflow-x-auto whitespace-nowrap flex-grow"> {/* 横スクロールと nowrap を追加、flex-growで残りのスペースを占める */}
+                        {/* 矢印のようなカードを横一直線に並べる */}
+                        {displayNumbers.map((num) => (
+                            <div 
+                                key={num} 
+                                className={`relative px-4 py-2 text-lg font-bold mr-1 flex items-center justify-center transition-all duration-300 transform skew-x-[-15deg] bg-gray-300 text-gray-600`}
+                                style={{ minWidth: '70px' }} // カードの最小幅を設定
+                            >
+                                <span className="relative z-10 transform skew-x-[15deg]">{num}</span> {/* 文字の傾きを戻す */}
+                                {/* 右側の三角部分 */}
+                                <div className={`absolute right-[-15px] top-0 w-0 h-0 border-t-[22px] border-b-[22px] border-l-[15px] bg-gray-300 border-l-gray-300 border-t-transparent border-b-transparent z-0`}></div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </div>
     );
